c94a47e275ccb086a888860b5181151598a62346
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-12-26 11:23:16 +0600

    added screen folder

---
+++ screens/CategoryMeals.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export const CategoryMeals = () => {
+  return (
+    <View style={styles.screen}>
+      <Text>Category Meals</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
