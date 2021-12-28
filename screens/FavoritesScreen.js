291ac249ac5db8a1b32830c78b66b239c9a21f55
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-12-28 16:46:33 +0600

    fallback text renders on FavoriteScreen

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,13 +1,25 @@
 // import React from "react";
 import { MealList } from "../components/MealList";
-import { MEALS } from "../data/dummy-data";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { CustomHeaderButton } from "../components/CustomHeaderButton";
 import { useSelector } from "react-redux";
+import { StyleSheet, Text, View } from "react-native";
+import { DefaultText } from "../components/DefaultText";
+import Colors from "../constants/Colors";
 
 export const FavoritesScreen = ({ navigation }) => {
   const favoriteMeals = useSelector((state) => state.meals.favoriteMeals);
 
+  if (favoriteMeals.length === 0 || !favoriteMeals) {
+    return (
+      <View style={styles.screen}>
+        <DefaultText>No favorite meals found.</DefaultText>
+        <DefaultText>
+          Start adding some as <Text style={styles.text}> FAVORITE! </Text>
+        </DefaultText>
+      </View>
+    );
+  }
   return <MealList listData={favoriteMeals} navigation={navigation} />;
 };
 
@@ -27,3 +39,15 @@ FavoritesScreen.navigationOptions = ({ navigation }) => {
     ),
   };
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontFamily: 'open-sans-bold',
+    color: Colors.primaryColor
+  }
+});
